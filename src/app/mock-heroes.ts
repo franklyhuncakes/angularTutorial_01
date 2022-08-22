6cf36451125da12815dc9adf349629d3d1235420
Aaron Mitchell <aaronm508@yahoo.com>
2022-08-22 07:00:51 -0500

    only development

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,23 @@
 import { Hero } from "./hero";
 
 export const HEROES: Hero[] = [
+    { id: 2, name: 'Sonic The Hedgehog' },
+    { id: 3, name: 'Tails' },
+    { id: 4, name: 'Spider Man' },
+    { id: 5, name: 'Cat Woman' },
+    { id: 6, name: 'Batman' },
+    { id: 7, name: 'Super Woman' },
+    { id: 8, name: 'Super Man' },
+    { id: 9, name: 'Wonder Woman' },
+    { id: 10, name: 'He Man' },
+    { id: 11, name: 'Iron Man' },
     { id: 12, name: 'Dr. Nice' },
     { id: 13, name: 'Bombasto' },
     { id: 14, name: 'Celeritas' },
     { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
+    { id: 16, name: 'Rubber Man' },
     { id: 17, name: 'Dynama' },
     { id: 18, name: 'Dr. IQ' },
     { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' },
+    { id: 20, name: 'Tornado' }
 ]
